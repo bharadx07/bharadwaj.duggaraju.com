9ac8262d8829b5762d78baf5bf874a161744aa27
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-07-10 20:06:36 -0700

    some final fixes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -75,7 +75,7 @@ const Contact: React.FC = () => {
             <ContactText>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ul. 
             </ContactText>
-            <ContactBTN>
+            <ContactBTN onClick={() => window.location.href="mailto:bharadwaj.duggaraju@outlook.com"}>
                 Get In Touch
             </ContactBTN>
         </ContactWrapper>
