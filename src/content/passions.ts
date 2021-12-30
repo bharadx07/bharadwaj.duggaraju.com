be4ca2769978f54bfb4e8a7bfed6f9eda9ff1801
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-12-30 10:54:19 -0800

    fix: typos and passion updates

--- src/content/passions.ts
+++ src/content/passions.ts
@@ -4,11 +4,11 @@ const PassionsContent = {
     "Through my learning, I have created many impactful projects and fun showcases. I have created projects ranging from <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://bdvideos.glitch.me`'>YouTube Clones</span> to <span style='color: orange; cursor: pointer;' onclick='window.location.href=`http://jagathifoundation.org`'>Nonprofit Websites</span>. I hope to make an impact through my programming.",
   ],
   Robotics: [
-    "Robotics has been one of my longest-running passions, as I started in 2016. My journey began by competing in the First Lego League, which I was a part of for 4 years. My team has had some great resuls such as placing 1st in our local competition, or being the winners of the Innovation award at the state competition.",
+    "Robotics has been one of my longest-running passions, as I started in 2016. My journey began by competing in the First Lego League, which I was a part of for 4 years. My team has had some great results such as placing 1st in our local competition, or being the winners of the Innovation award at the state competition.",
     "Moving into High School, I have joined my school's FRC team. I teach programming and robotics through <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://roboticsforall.us`'>RFA</span>, and work on various programming/mech projects through my school's robotics team.",
   ],
   Cubing: [
-    "Cubing or Speedcubing has become one of my most fun passions. It involves scrambling and solving a variety of Rubik Cubes in the fastest time possible (For me 10 seconds).",
+    "Cubing or Speedcubing has become one of my most fun pastimes. It involves scrambling and solving a variety of Rubik Cubes in the fastest time possible (For me 9 seconds). I placed 3rd at the Rubik Washington State Championships in 2018. I am also ranked 2nd in India for the Square-1 event.",
     "I enjoy spending time honing my skills at home, collecting different cubes, and attending <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://www.worldcubeassociation.org/persons/2017DUGG01`'>Cubing Competitions</span>. I have made many friends through cubing, and hope to consistently improve.",
   ],
   Debate: [
@@ -17,7 +17,7 @@ const PassionsContent = {
   ],
   Badminton: [
     "Since many of my passions involve the computer, Badminton has become a unique passion for me. It is almost like a break for me and helps me relax. I have played the sport for 5+ years but only started training 1 year ago.",
-    "I enjoy playing casually with my friends, and try-harding in games. I play both doubles and singles and hope to attend tournaments once COVID ends.",
+    "I enjoy playing casually with my friends, and try-harding in games. I play both doubles and singles and attend tournaments for fun at my local club.",
   ],
 };
 
