3bf828a621c5bfb946859e81d19dd226825bb40a
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-12-30 10:42:54 -0800

    feat: robotics and debate to the prime 5 passions

--- src/content/passions.ts
+++ src/content/passions.ts
@@ -1,27 +1,24 @@
 const PassionsContent = {
-    Programming: [
-        "My passion for programming started in 2016 through Scratch. Since then, I have learned Python, Javascript, Java, CSS, HTML, React, Typescript and more. I strive to learn and expand my skills.",
-        "Through my learning, I have created many impactful projects and fun showcases. I have created projects ranging from <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://bdvideos.glitch.me`'>YouTube Clones</span> to <span style='color: orange; cursor: pointer;' onclick='window.location.href=`http://jagathifoundation.org`'>Nonprofit Websites</span>. I hope to make an impact through my programming."
-    ],
-    Volunteering: [
-        "Volunteering is one of my strongest pastimes, as it allows me to impact the lives of others. I love helping people who need it and putting a smile on people's faces. It makes my day.",
-        "Along with serving food and resources to help those, I strive to use my programming skills to help. I teach programming through <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://roboticsforall.us`'>RFA</span>, work on the <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://github.com/bharadwajduggaraju/JF-Nonprofit`'>Jagathi Foundation NP</span> and start <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://bellevuefooddrive.glitch.me`'>Food Drives</span> in my neighborhood."
-    ],
-    Cubing: [
-        "Cubing or Speedcubing has become one of my most fun passions. It involves scrambling and solving a variety of Rubik Cubes in the fastest time possible (For me 10 seconds).",
-        "I enjoy spending time honing my skills at home, collecting different cubes, and attending <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://www.worldcubeassociation.org/persons/2017DUGG01`'>Cubing Competitions</span>. I have made many friends through cubing, and hope to consistently improve."
-    ], 
-    Writing: [
-        "Writing has become one of my passions in recent times. I used to dread writing, and any sort of writing assignment would anger me. However as 8th grade passed, I found enjoyment in writing.",
-        "Since then, I have enjoyed creative writing, writing logs, and honing my writing skills. I hope that my focus on writing will help me excel in school."
-    ],
-    Badminton: [
-        "Since many of my passions involve the computer, Badminton has become a unique passion for me. It is almost like a break for me and helps me relax. I have played the sport for 5+ years but only started training 1 year ago.",
-        "I enjoy playing casually with my friends, and try-harding in games. I play both doubles and singles and hope to attend tournaments once COVID ends."
-    ]
-}
+  Programming: [
+    "My passion for programming started in 2016 through Scratch. Since then, I have learned Python, Javascript, Java, CSS, HTML, React, Typescript and more. I strive to learn and expand my skills.",
+    "Through my learning, I have created many impactful projects and fun showcases. I have created projects ranging from <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://bdvideos.glitch.me`'>YouTube Clones</span> to <span style='color: orange; cursor: pointer;' onclick='window.location.href=`http://jagathifoundation.org`'>Nonprofit Websites</span>. I hope to make an impact through my programming.",
+  ],
+  Robotics: [
+    "Robotics has been one of my longest-running passions, as I started in 2016. My journey began by competing in the First Lego League, which I was a part of for 4 years. My team has had some great resuls such as placing 1st in our local competition, or being the winners of the Innovation award at the state competition.",
+    "Moving into High School, I have joined my school's FRC team. I teach programming and robotics through <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://roboticsforall.us`'>RFA</span>, and work on various programming/mech projects through my school's robotics team.",
+  ],
+  Cubing: [
+    "Cubing or Speedcubing has become one of my most fun passions. It involves scrambling and solving a variety of Rubik Cubes in the fastest time possible (For me 10 seconds).",
+    "I enjoy spending time honing my skills at home, collecting different cubes, and attending <span style='color: orange; cursor: pointer;' onclick='window.location.href=`https://www.worldcubeassociation.org/persons/2017DUGG01`'>Cubing Competitions</span>. I have made many friends through cubing, and hope to consistently improve.",
+  ],
+  Debate: [
+    "Debate has become one of my top passions in recent times. I began my Debate journey in 6th grade, with Debate I. As 8th grade passed, I decided to take Debate II to expand my debate knowledge.",
+    "Since HS has started, joining the Debate club as allowed me to hone my skills and compete in leagues. I'm focusing on Public Forum Debate, and hope to break at upcoming competitions!",
+  ],
+  Badminton: [
+    "Since many of my passions involve the computer, Badminton has become a unique passion for me. It is almost like a break for me and helps me relax. I have played the sport for 5+ years but only started training 1 year ago.",
+    "I enjoy playing casually with my friends, and try-harding in games. I play both doubles and singles and hope to attend tournaments once COVID ends.",
+  ],
+};
 
-
-
-
-export default PassionsContent
+export default PassionsContent;
